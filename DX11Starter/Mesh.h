58c5f2900d73791e0902138249f4336b610eacc0
gll1460 <gll1460@IGM2000-29>
2019-09-03 15:36:15 -0400

    Stuff added

---
+++ DX11Starter/Mesh.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <d3d11.h>
+
+#include "Vertex.h"
+class Mesh
+{
+private:
+	//Buffer pointers
+	ID3D11Buffer* vertexBuffer = nullptr;
+	ID3D11Buffer* indexBuffer = nullptr;
+
+	//Integer specifying how many indices are in the mesh's index buffer
+	int meshIndices;
+
+public:
+	//Constructor
+	Mesh(Vertex* vertices, int vertexCount, UINT* indices, int indexCount, ID3D11Device* bufferCreator);
+
+	//Destructor
+	virtual ~Mesh();
+
+	ID3D11Buffer* GetVertexBuffer();
+	ID3D11Buffer* GetIndexBuffer();
+	int GetIndexCount();
+};
+
