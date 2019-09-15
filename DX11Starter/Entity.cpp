f20e0e30255f4b1102514595c7138df2190a7ae5
gll1460 <gll1460@IGM2000-29>
2019-09-15 16:45:14 -0400

    Idk

--- DX11Starter/Entity.cpp
+++ DX11Starter/Entity.cpp
@@ -4,23 +4,31 @@
 Entity::Entity(Mesh* meshPtr)
 {
 	object = meshPtr;
-	XMStoreFloat4x4(&worldMatrix, XMMatrixIdentity());
-	position = XMFLOAT3(0.0f, 0.0f, 0.0f);
-	scale = XMFLOAT3(1.0f, 1.0f, 1.0f);
-	rotation = XMFLOAT3(0.0f, 0.0f, 0.0f);
+	XMStoreFloat4x4(&worldMatrix, DirectX::XMMatrixIdentity());
+	position = DirectX::XMFLOAT3(0.0f, 0.0f, 0.0f);
+	scale = DirectX::XMFLOAT3(1.0f, 1.0f, 1.0f);
+	rotation = DirectX::XMFLOAT3(0.0f, 0.0f, 0.0f);
 }
 
 //Accessors
-XMFLOAT4X4 Entity::GetWorldMatrix() { return worldMatrix; }
-void Entity::SetWorldMatrix(XMFLOAT4X4 wM) { worldMatrix = wM; }
+DirectX::XMFLOAT4X4 Entity::GetWorldMatrix() { return worldMatrix; }
+void Entity::SetWorldMatrix(DirectX::XMFLOAT4X4 wM) { worldMatrix = wM; }
 
-XMFLOAT3 Entity::GetPosition() { return position; }
-void Entity::SetPostion(XMFLOAT3 pos) {	position = pos; }
+DirectX::XMFLOAT3 Entity::GetPosition() { return position; }
+void Entity::SetPostion(DirectX::XMFLOAT3 pos) {	position = pos; }
 
-XMFLOAT3 Entity::GetScale() { return scale; }
-void Entity::SetScale(XMFLOAT3 scl) { scale = scl; }
+DirectX::XMFLOAT3 Entity::GetScale() { return scale; }
+void Entity::SetScale(DirectX::XMFLOAT3 scl) { scale = scl; }
 
-XMFLOAT3 Entity::GetRotation() { return rotation; }
-void Entity::SetRotation(XMFLOAT3 rot) { rotation = rot; }
+DirectX::XMFLOAT3 Entity::GetRotation() { return rotation; }
+void Entity::SetRotation(DirectX::XMFLOAT3 rot) { rotation = rot; }
 
+void Entity::Move(float x, float y, float z)
+{
+	DirectX::XMFLOAT3 currentPos = position;
+	position = DirectX::XMFLOAT3(currentPos.x + x, currentPos.y + y, currentPos.z + z);
+}
 
+Entity::~Entity()
+{
+}
--- DX11Starter/Entity.h
+++ DX11Starter/Entity.h
@@ -4,30 +4,31 @@
 
 #include "Mesh.h"
 
-using namespace DirectX;
-
 class Entity
 {
 private:
-	XMFLOAT4X4 worldMatrix;
-	XMFLOAT3 position;
-	XMFLOAT3 scale;
-	XMFLOAT3 rotation;
+	DirectX::XMFLOAT4X4 worldMatrix;
+	DirectX::XMFLOAT3 position;
+	DirectX::XMFLOAT3 scale;
+	DirectX::XMFLOAT3 rotation;
 	Mesh* object;
 public:
 	//Constructor
 	Entity(Mesh* meshPtr);
 
 	//Accessors
-	XMFLOAT4X4 GetWorldMatrix();
-	void SetWorldMatrix(XMFLOAT4X4 wM);
-	XMFLOAT3 GetPosition();
-	void SetPostion(XMFLOAT3 pos);
-	XMFLOAT3 GetScale();
-	void SetScale(XMFLOAT3 scl);
-	XMFLOAT3 GetRotation();
-	void SetRotation(XMFLOAT3 rot);
+	DirectX::XMFLOAT4X4 GetWorldMatrix();
+	void SetWorldMatrix(DirectX::XMFLOAT4X4 wM);
+	DirectX::XMFLOAT3 GetPosition();
+	void SetPostion(DirectX::XMFLOAT3 pos);
+	DirectX::XMFLOAT3 GetScale();
+	void SetScale(DirectX::XMFLOAT3 scl);
+	DirectX::XMFLOAT3 GetRotation();
+	void SetRotation(DirectX::XMFLOAT3 rot);
 
-	void Move(float distance, float direction);
+	//Method to move entity
+	void Move(float x, float y, float z);
+
+	//Destructor
+	virtual ~Entity();
 };
-
